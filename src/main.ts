ee54d550245d01dd750850c8d6cfb961a38a5ea1
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-23 07:21:13 -0800

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('TD-OLLAMA API')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions =  {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+
+  function documentFactory() {
+    return SwaggerModule.createDocument(app, config, options);
+  }
+
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
