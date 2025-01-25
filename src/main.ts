f3c338de4b9589335101d444bee6e769566d642d
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-24 17:43:02 -0800

    improve config

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,9 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
 
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
   setupSwagger(app);
 
   await app.listen(process.env.PORT ?? 3000);
