b4d97afd14b7bb4ac34abae5ab940716a9a5e528
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-26 04:31:46 -0800

    global modules does not work inside docker for some reason

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AtStrategy, RtStrategy } from './strategies';
 import { AuthResolver } from './auth.resolver';
+import { PrismaModule } from '~/prisma/prisma.module';
 
 @Module({
-  imports: [JwtModule.register({})],
+  imports: [JwtModule.register({}), PrismaModule],
   providers: [AuthService, AtStrategy, RtStrategy, AuthResolver],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserService, PresetsService } from './user.service';
 import { UserResolver, PresetsResolver } from './user.resolver';
+import { PrismaModule } from '~/prisma/prisma.module';
 
 @Module({
   providers: [UserResolver, PresetsResolver, UserService, PresetsService],
+  imports: [PrismaModule],
 })
 export class UserModule {}
