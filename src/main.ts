d4c16650f07f2b654ccef5163539bed4e9e5e9e8
wladyslaw <vladislavvaliuchenkov@gmail.com>
2025-01-26 04:33:02 -0800

    add docker

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, process.env.HOST ?? 'localhost');
 }
 
 void bootstrap();
